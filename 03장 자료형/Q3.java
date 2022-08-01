a6997de37f59afbc8eb8a92920173b232045df8f
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-08-01 09:53:03 +0900

    Create Q3.java

---
+++ 03장 자료형/Q3.java
@@ -0,0 +1,10 @@
+public class JumpToJava {
+    public static void main(String[] args) {
+        String id = "881120-1068234";
+        String first = id.substring(0, 6);
+        String second = id.substring(7);
+
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
