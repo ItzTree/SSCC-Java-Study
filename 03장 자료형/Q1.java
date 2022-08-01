dab48e1f00adc3678454abe654845815f25c3d6a
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-08-01 09:47:47 +0900

    Create Q1.java

---
+++ 03장 자료형/Q1.java
@@ -0,0 +1,8 @@
+public class JumpToJava {
+    public static void main(String[] args) {
+        int kor = 80;
+        int eng = 75;
+        int mat = 55;
+        System.out.println((kor + eng + mat) / 3);
+    }
+}
