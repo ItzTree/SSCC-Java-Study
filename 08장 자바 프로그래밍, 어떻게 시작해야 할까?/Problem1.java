89ed6439fac3406ef7f7f561dcd536678cf97614
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-08-09 13:35:27 +0900

    Create Problem1.java

---
+++ 08장 자바 프로그래밍, 어떻게 시작해야 할까?/Problem1.java
@@ -0,0 +1,15 @@
+public class Problem1 {
+    public static void main(String[] args) {
+        int n = 1;
+        int sum = 0;
+
+        while (n < 1000) {
+            if ((n % 3 == 0) || (n % 5 == 0)) {
+                sum += n;
+            }
+            n++;
+        }
+
+        System.out.println(sum);
+    }
+}
