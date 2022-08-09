218c462dab1966d4426186bfe0b81d1b59b8fff3
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-08-09 12:37:45 +0900

    Create Q1.java

---
+++ 07장 자바 날개 달기/Q1.java
@@ -0,0 +1,8 @@
+import house.HousePark;
+
+public class JumpToJava {
+    public static void main(String[] args) {
+        HousePark housePark = new HousePark();
+        System.out.println(housePark.getLastname());
+    }
+}
